33bb088ca5baac427cd21cf1012ef9d3fe3c6f88
Htet Lynn Htun <htetlynnhtun101@gmail.com>
2023-03-31 15:11:22 +0630

    Create `Movie` model

---
+++ ModularTMDB/ModularTMDB/Movie Feature/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  ModularTMDB
+//
+//  Created by Htet LynnHtun on 31/03/2023.
+//
+
+import Foundation
+
+struct Movie {
+    let id: Int
+    let title: String
+    let posterPath: String
+}
